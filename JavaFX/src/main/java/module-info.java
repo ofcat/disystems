8aa771220a9623cb96a086f77bf45aa82ddae494
ofcat <klemenko.v@hotmail.com>
2022-08-19 17:11:28 +0200

    Initial commit

---
+++ JavaFX/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.example.javafx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.net.http;
+    requires org.json;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.javafx to javafx.fxml;
+    exports com.example.javafx;
+}
